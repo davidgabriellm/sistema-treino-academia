d2dd288a24ddcf9e7962104c3dd745f7137c438f
davidgabriellm <davidglm.2002@gmail.com>
2026-03-02 13:06:26 -0300

    feat: add fastify api

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,18 @@
-console.log("Hello, World!");
+import "dotenv/config"
+import Fastify from 'fastify'
 
+const fastify = Fastify({
+  logger: true
+})
+
+
+fastify.get('/', function (request, reply) {
+  reply.send({ hello: 'worldavid' })
+})
+
+fastify.listen({ port: Number(process.env.PORT) ?? 8081}, function (err, address) {
+  if (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+})
